6b93263c39abbe4c49ee174e8c848852ab37dc91
djob195 <djorellanab@outlook.com>
2022-01-24 10:12:19 -0600

    fix 60 seconds timeout[

--- src/services/epay.service.js
+++ src/services/epay.service.js
@@ -1,13 +1,15 @@
 import fetch from "node-fetch";
+import AbortController from "abort-controller";
 import { EpayConstant } from "../constants/index.js";
 import { EpayModel } from "../models/index.js";
+const TIME_EPAY = 60000;
 
 const serviceInvoice = async (body, action = 1) => {
   const invoiceUrl = process.env.INVOICE_URL;
   const res = await fetch(`${invoiceUrl}/api/epay?action=${action}`, {
     method: "POST",
     body: JSON.stringify(body),
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" }
   });
   if (!res.ok) {
     throw new Error("Error en la comunicación");
@@ -23,22 +25,34 @@ const serviceInvoice = async (body, action = 1) => {
 };
 
 const serviceEpay = async (model, action) => {
+  const controller = new AbortController();
+  const timeout = setTimeout(() => {
+    controller.abort();
+  }, TIME_EPAY);
+  let hasError = false;
+  let res = null;
+
   const epayUrl = process.env.EPAY_URL;
   let xml = EpayModel.modelToXml(model, action);
   const option = {
+    signal: controller.signal,
     method: "POST",
     body: xml,
     headers: {
-      "Content-Type": "text/xml; charset=utf-8",
-    },
+      "Content-Type": "text/xml; charset=utf-8"
+    }
   };
-  let res = null;
   try {
     res = await fetch(`${epayUrl}?WSDL`, option);
   } catch (_error) {
     xml = EpayModel.modelToXml(model, action, true);
     option.body = xml;
     await fetch(`${epayUrl}?WSDL`, option);
+    hasError = true;
+  } finally {
+    clearTimeout(timeout);
+  }
+  if (hasError) {
     throw new Error("Error en la comunicación del sevicios de cobros");
   }
   if (!res.ok) {
@@ -59,5 +73,5 @@ const serviceEpay = async (model, action) => {
 
 export default {
   serviceInvoice,
-  serviceEpay,
+  serviceEpay
 };
